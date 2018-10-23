c0ec60d88d4a53bb2a8c28a95642d0537b6780f3
Kaleb Allen <allen.henry.kaleb@.com>
2018-10-23 13:45:31 -0500

    added code

--- LyricsObtainer/ViewController.swift
+++ LyricsObtainer/ViewController.swift
@@ -7,14 +7,57 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var ArtistText: UITextField!
+    @IBOutlet weak var SongTitle: UITextField!
+    @IBOutlet weak var LyricsFeild: UITextView!
+    @IBOutlet weak var SubmitButton: UIButton!
+    
+    let lyricsAPIBaseURL = "https://api.lyrics.ovh/v1/"
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-
+    @IBAction func
+        submiteButtonTapped(_ sender: Any) {
+        
+        guard let artistName = ArtistText.text,
+            let songTitle = SongTitle.text else {
+                return
+        }
+        
+        let artistNameURLComponet = artistName.replacingOccurrences(of: " ", with: "+")
+        
+        
+        let songTitleURLComponet = songTitle.replacingOccurrences(of: " ", with: "+")
+        
+        
+        //Full URL for the request we will make to the API
+        let requestURL = lyricsAPIBaseURL + artistNameURLComponet + "/" + songTitleURLComponet
+        //using Alamo fire to create an actual request
+        let request = Alamofire.request(requestURL, method: .get, parameters: nil, encoding: JSONEncoding.default, headers: nil)
+        //now we actually got to make our request
+        request.responseJSON { response in
+            //we switch based on wather we scucced or not
+            switch response.result {
+            case .success(let value):
+                let json = JSON(value)
+                print(json)
+                self.LyricsFeild.text = json["lyrics"].stringValue
+                
+                //in the case of suceed and we've gottne data
+                print("sucess!")
+            case .failure(let error):
+                //in the case of a failure the request did not get data back
+                print("error:(")
+            }
+        }
+    }
+    
 }
 
---
+++ Pods/Target Support Files/Pods-LyricsObtainer/Pods-LyricsObtainer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_LyricsObtainer : NSObject
+@end
+@implementation PodsDummy_Pods_LyricsObtainer
+@end
